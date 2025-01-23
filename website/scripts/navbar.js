351907ad36629b4a86a18f4ea6384d0494161f8a
dLeczycki <danielleczycki@gmail.com>
2025-01-23 13:21:04 +0100

    Improve header responsiveness

--- website/scripts/navbar.js
+++ website/scripts/navbar.js
@@ -1,10 +1,12 @@
+const SCROLL_MAX = 200;
+const NAVBAR_OFFSET = 70;
+
 const navbar = document.querySelector('nav#navbar');
 const scrollLinks = document.querySelectorAll('a.menu-link');
-const scrollMax = 200;
-const navbarOffset = 70;
+const hamburger = document.querySelector('button#hamburger');
 
 const scrollWithOpacity = () => {
-    const opacity = window.scrollY > scrollMax ? 1 : 1 - (scrollMax - window.scrollY) / scrollMax;
+    const opacity = window.scrollY > SCROLL_MAX ? 1 : 1 - (SCROLL_MAX - window.scrollY) / SCROLL_MAX;
     navbar.style.backgroundColor = `rgba(0,0,0,${opacity})`;
 }
 
@@ -13,12 +15,18 @@ const moveToSection = (e) => {
 
     const sectionId = e.target.getAttribute('href');
     const targetSection = document.querySelector(sectionId);
-    const sectionPosition = targetSection.offsetTop - navbarOffset;
+    const sectionPosition = targetSection.offsetTop - NAVBAR_OFFSET;
 
     window.scrollTo({
         top: sectionPosition,
         behavior: 'smooth',
     });
+}
+
+const toggleHamburger = (e) => {
+    e.preventDefault();
+
+    hamburger.classList.toggle('active');
 }
 
 
@@ -27,4 +35,6 @@ window.addEventListener('scroll', scrollWithOpacity);
 for (link of scrollLinks) {
     link.addEventListener('click', moveToSection)
 }
+
+hamburger.addEventListener('click', toggleHamburger);
 
